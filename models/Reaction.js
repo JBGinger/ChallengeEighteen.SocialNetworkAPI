549df7b08ae4903d4ba1906ad3fd905e144d6f81
jeffb <jeffb@bsdiweb.com>
2022-01-09 14:30:42 -0500

    Added reaction ID field back

--- models/Reaction.js
+++ models/Reaction.js
@@ -3,6 +3,10 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReactionSchema = new Schema(
     {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
         reactionBody: {
             type: String,
             required: true,
